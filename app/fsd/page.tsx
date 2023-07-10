916c92678fad9ea3610f0f46d07b5e97cf724ce0
hari <harischetan717@gmail.com>
2023-07-10 08:51:56 -0700

    check1

--- app/fsd/page.tsx
+++ app/fsd/page.tsx
@@ -2,3 +2,10 @@
 // import 'aos/dist/aos.css'; // You can also use <link> for styles
 // // ..
 // AOS.init();
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>page</div>
+  )
+}
